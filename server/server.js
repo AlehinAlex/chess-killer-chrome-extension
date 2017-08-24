c9f8246f6cf523f785e0421a0d0fd8fd8d407ada
pavolbincik <pavolbinck@gmail.com>
2017-08-24 21:16:02 +0200

    add index

--- server/server.js
+++ server/server.js
@@ -4,82 +4,66 @@ const fs = require('fs');
 const PORT = 5000;
 const spawn = require('child_process').spawn;
 
-const EngineConnect = function (cmd, args) {
-    this.cmd = cmd;
-    this.args = args;
-}
 
-EngineConnect.prototype.run = function (fen, delay, cb) {
-    this.result = '';
-    if (this.loop) {
-        clearInterval(this.loop);
+class EngineConnect {
+    constructor(cmd, args) {
+        this.cmd = cmd;
+        this.args = args;
     }
 
-    const child = spawn(this.cmd, this.args);
+    run(fen, delay, cb) {
+        let result = '';
+        if (this.loop) {
+            clearInterval(this.loop);
+        }
 
-    child.stdout.on('data', function (buffer) {
-        this.stdout += buffer.toString();
-        result = this.stdout;
-    });
+        const child = spawn(this.cmd, this.args);
 
-    child.stdout.on('end', function () {
-        cb(this.stdout);
-    });
+        child.stdout.on('data', function (buffer) {
+            this.stdout += buffer.toString();
+            result = this.stdout;
+        });
 
-    child.stdin.write("position fen " + fen + "\ngo movetime " + delay + "\n");
+        child.stdout.on('end', function () {
+            cb(this.stdout);
+        });
 
-    this.loop = setInterval(() => {
-        cb(result);
-    }, 1000);
+        child.stdin.write("position fen " + fen + "\ngo movetime " + delay + "\n");
+
+        this.loop = setInterval(() => {
+            cb(result);
+        }, 1000);
+    }
 }
+
 
 const httpsOptions = {
     key: fs.readFileSync(__dirname + '/key.pem'),
     cert: fs.readFileSync(__dirname + '/cert.pem')
 };
 
-const server = https.createServer(httpsOptions, app).listen(PORT, () => {
-    console.log('server running at ' + PORT);
+const server = https.createServer(httpsOptions, app);
+
+server.listen(PORT, () => {
+    console.log('Server running at ' + PORT);
 });
 
 const io = require('socket.io')(server);
 
 let loop;
 
-function runCmd(cmd, args, fen, delay, cb) {
-    if (loop) {
-        clearInterval(loop);
-    }
-
-    const spawn = require('child_process').spawn,
-        child = spawn(cmd, args);
-
-    let result = '';
-    child.stdout.on('data', function (buffer) {
-        this.stdout += buffer.toString();
-        result = this.stdout;
-    });
-
-    child.stdout.on('end', function () {
-        cb(this.stdout);
-    });
-
-    child.stdin.write("position fen " + fen + "\ngo movetime " + delay + "\n");
-
-    loop = setInterval(() => {
-        cb(result);
-    }, 1000);
-}
-
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/public/socket.html');
+    res.sendFile(__dirname + '/public/index.html');
 });
+
+
+let engine;
 
 io.on('connection', (socket) => {
     console.log('a user connected', socket.id);
 
     socket.on('new_move', (data) => {
-        const engine = new EngineConnect(__dirname + '/stockfish', []);
+        engine = new EngineConnect(__dirname + '/stockfish', []);
         engine.run(data.FEN, 20000, (bestmove) => {
             socket.emit('on_result', {fen: data.FEN, data: bestmove});
         });
